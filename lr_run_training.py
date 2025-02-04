5d344a4d422abbe2800e13834535ecdc949974ce
m_ichaels <m57sharp@gmail.com>
2025-02-04 21:04:17 +0000

    watch me mum

---
+++ lr_run_training.py
@@ -0,0 +1,130 @@
+import yfinance as yf
+import sys
+import os
+import requests
+import stock_utils
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import argparse
+import pickle
+import matplotlib.pyplot as plt
+from sklearn.impute import SimpleImputer
+import math
+
+class LR_training:
+    def __init__(self, model_version, threshold = 0.98, start_date = None, end_date = None):
+        self.model_version = model_version
+        self.threshold = threshold
+
+        if start_date:
+            self.start_date = start_date
+        if end_date:
+            self.end_date = end_date
+
+        stocks = ["SPY"]
+        self.stocks = list(np.unique(stocks))
+
+        self.main_df = pd.DataFrame(columns = ['volume', 'normalized_value', '3_reg', '5_reg', '10_reg', '20_reg', 'target'])
+
+        self.scaler = MinMaxScaler()
+        self.lr = LogisticRegression()
+
+        self.fetch_data()
+        self.create_train_test()
+        self.fit_model()
+        self.confusion_matrix()
+        self.save_model()
+
+    def fetch_data(self):
+        """
+        fetch train and test data from yahoo finance
+        """
+        for stock in self.stocks:
+            try:
+                df = stock_utils.create_train_data(stock, n = 10)
+                self.main_df = pd.concat([self.main_df, df])
+            except:
+                pass
+        print(f'{len(self.main_df)} samples were fetched from the database.')
+    
+    def create_train_test(self):
+        """
+        create train and test data
+        """
+        self.main_df = self.main_df.sample(frac = 1, random_state = 3). reset_index(drop = True)
+        print(self.main_df.columns)
+        self.main_df = self.main_df.dropna(axis=1, how='all')
+        print(self.main_df.columns)
+        self.main_df.columns = [col[0] if isinstance(col, tuple) else col for col in self.main_df.columns]
+        print(self.main_df.columns)
+        self.main_df['target'] = self.main_df['target'].astype('category')
+        print(self.main_df)
+        self.main_df.columns = self.main_df.columns.astype(str)
+
+        y = self.main_df.pop('target').to_numpy()
+        y = y.reshape(y.shape[0], 1)
+        x = self.scaler.fit_transform(self.main_df)
+
+        #test train split
+        self.train_x, self.test_x, self.train_y, self.test_y = train_test_split(x, y, \
+            test_size = 0.05, random_state = 50, shuffle = True)
+        print('Created test and train data...')
+
+    def fit_model(self):
+
+        print('Training model...')
+        self.lr.fit(self.train_x, self.train_y)
+        
+        #predict the test data
+        self.predictions = self.lr.predict(self.test_x)
+        self.score = self.lr.score(self.test_x, self.test_y)
+        print(f'Logistic regression model score: {self.score}')
+
+        #preds with threshold
+        self.predictions_proba = self.lr._predict_proba_lr(self.test_x)
+        self.predictions_proba_thresholded = self._threshold(self.predictions_proba, self.threshold)
+
+    def confusion_matrix(self):
+        cm = confusion_matrix(self.test_y, self.predictions)
+        self.cmd = ConfusionMatrixDisplay(cm)
+        
+        cm_thresholded = confusion_matrix(self.test_y, self.predictions_proba_thresholded)
+        self.cmd_thresholded = ConfusionMatrixDisplay(cm_thresholded)
+
+    def _threshold(self, predictions, threshold):
+
+        prob_thresholded = [0 if x > threshold else 1 for x in predictions[:, 0]]
+
+        return np.array(prob_thresholded)
+
+    def save_model(self):
+
+        #save models
+        saved_models_dir = os.path.join(os.getcwd(), 'saved_models')
+        model_file = f'lr_{self.model_version}.sav'
+        model_dir = os.path.join(saved_models_dir, model_file)
+        pickle.dump(self.lr, open(model_dir, 'wb'))
+
+        scaler_file = f'scaler_{self.model_version}.sav'
+        scaler_dir = os.path.join(saved_models_dir, scaler_file)
+        pickle.dump(self.scaler, open(scaler_dir, 'wb'))
+
+        print(f'Saved the model and scaler in {saved_models_dir}')
+        cm_path = os.path.join(os.getcwd(), 'results\Confusion Matrices')
+        
+        #save cms
+        plt.figure()
+        self.cmd.plot()
+        plt.savefig(f'{cm_path}\\cm_{self.model_version}.jpg')
+
+        plt.figure()
+        self.cmd_thresholded.plot()
+        plt.savefig(f'{cm_path}\\cm_thresholded_{self.model_version}.jpg')
+        print(f'Figures saved in {cm_path}')
+
+if __name__ == "__main__":
+    run_lr = LR_training('v2')
---
+++ stock_utils.py
@@ -0,0 +1,157 @@
+import yfinance as yf
+from datetime import datetime, timezone, timedelta
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+from scipy.signal import argrelextrema
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+
+def timestamp(dt):
+    epoch = datetime.fromtimestamp(0, tz=timezone.utc)  # Corrected for timezone awareness
+    return int((dt - epoch).total_seconds() * 1000)
+
+def linear_regression(x, y):
+    """
+    performs linear regression given x and y. outputs regression coefficient
+    """
+    #fit linear regression
+    lr = LinearRegression()
+    lr.fit(x, y)
+
+    return lr.coef_[0][0]
+
+def n_day_regression(n, df, idxs):
+    """
+    n day regression.
+    """
+    #variable
+    _varname_ = f'{n}_reg'
+    df[_varname_] = np.nan
+
+    for idx in idxs:
+        if idx > n:
+            
+            y = df['Close'][idx - n: idx].to_numpy()
+            x = np.arange(0, n)
+            #reshape
+            y = y.reshape(y.shape[0], 1)
+            x = x.reshape(x.shape[0], 1)
+            #calculate regression coefficient 
+            coef = linear_regression(x, y)
+            df.loc[idx, _varname_] = coef #add the new value
+            
+    return df
+
+def normalized_values(high, low, close):
+    """
+    normalize the price between 0 and 1.
+    """
+    #epsilon to avoid deletion by 0
+    epsilon = 10e-10
+    
+    #subtract the lows
+    
+    high = high - low
+    close = close - low
+    return close/(high + epsilon)
+
+def get_stock_price(stock, date):
+    """
+    returns the stock price given a date
+    """
+    start_date = date - timedelta(days = 10)
+    end_date = date
+    data = yf.download(stock, start=start_date, end=end_date)
+    data.reset_index(inplace=True)
+    
+    return data['Close'].values[-1]
+
+def get_data(stock, start_date = None, end_date = None, n = 10):
+    data = yf.download(stock, start=start_date, end=end_date)
+    data.reset_index(inplace=True)
+    data =pd.DataFrame(data)
+    #add the noramlzied value function and create a new column
+    data['normalized_value'] = data.apply(lambda x: normalized_values(x['High'], x['Low'], x['Close']), axis = 1)
+    #column with local minima and maxima
+    data['loc_min'] = data.iloc[argrelextrema(data['Close'].values, np.less_equal, order = n)[0]]['Close']
+    data['loc_max'] = data.iloc[argrelextrema(data['Close'].values, np.greater_equal, order = n)[0]]['Close']
+
+    #idx with mins and max
+    idx_with_mins = np.where(data['loc_min'] > 0)[0]
+    idx_with_maxs = np.where(data['loc_max'] > 0)[0]
+    
+    return data, idx_with_mins, idx_with_maxs
+
+def create_train_data(stock, start_date = None, end_date = None, n = 10):
+
+    #get data to a dataframe
+    data, idxs_with_mins, idxs_with_maxs = get_data(stock, start_date, end_date, n)
+    
+    #create regressions for 3, 5 and 10 days
+    data = n_day_regression(3, data, list(idxs_with_mins) + list(idxs_with_maxs))
+    data = n_day_regression(5, data, list(idxs_with_mins) + list(idxs_with_maxs))
+    data = n_day_regression(10, data, list(idxs_with_mins) + list(idxs_with_maxs))
+    data = n_day_regression(20, data, list(idxs_with_mins) + list(idxs_with_maxs))
+  
+    _data_ = data[(data['loc_min'] > 0) | (data['loc_max'] > 0)].reset_index(drop = True)
+    
+    #create a dummy variable for local_min (0) and max (1)
+    _data_['target'] = [1 if x > 0 else 0 for x in _data_['loc_max']]
+    #columns of interest
+    _data_ = _data_.rename(columns={'Close': 'close', 'Volume': 'volume'})
+    cols_of_interest = ['volume', 'normalized_value', '3_reg', '5_reg', '10_reg', '20_reg', 'target']
+    _data_ = _data_[cols_of_interest]
+    return _data_.dropna(axis = 0)
+
+def create_test_data_lr(stock, start_date = None, end_date = None, n = 10):
+    """
+    this function create test data sample for logistic regression model
+    """
+    #get data to a dataframe
+    data, _, _ = get_data(stock, start_date, end_date, n)
+    idxs = np.arange(0, len(data))
+    
+    #create regressions for 3, 5 and 10 days
+    data = n_day_regression(3, data, idxs)
+    data = n_day_regression(5, data, idxs)
+    data = n_day_regression(10, data, idxs)
+    data = n_day_regression(20, data, idxs)
+
+    data = data.rename(columns={'Close': 'close', 'Volume': 'volume'})
+    cols = ['close', 'volume', 'normalized_value', '3_reg', '5_reg', '10_reg', '20_reg']
+    data = data[cols]
+
+    return data.dropna(axis = 0)
+
+def predict_trend(stock, _model_, start_date = None, end_date = None, n = 10):
+
+    #get data to a dataframe
+    data, _, _ = get_data(stock, start_date, end_date, n)
+    
+    idxs = np.arange(0, len(data))
+    #create regressions for 3, 5 and 10 days
+    data = n_day_regression(3, data, idxs)
+    data = n_day_regression(5, data, idxs)
+    data = n_day_regression(10, data, idxs)
+    data = n_day_regression(20, data, idxs)
+        
+    #create a column for predicted value
+    data['pred'] = np.nan
+
+    #get data
+    cols = ['volume', 'normalized_value', '3_reg', '5_reg', '10_reg', '20_reg']
+    x = data[cols]
+
+    #scale the x data
+    scaler = MinMaxScaler()
+    x = scaler.fit_transform(x)
+
+    for i in range(x.shape[0]):
+        
+        try:
+            data['pred'][i] = _model_.predict(x[i, :])
+
+        except:
+            data['pred'][i] = np.nan
+
+    return data
